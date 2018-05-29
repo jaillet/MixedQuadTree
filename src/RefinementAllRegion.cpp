5c8337c354a13c1b0a16488c7330da0fd72efa66
Fabrice Jaillet <fjaillet@gmail.com>
2018-05-29 17:17:22 -0300

    FJA list initialisation in Services/Refinement* constructors.

--- src/RefinementAllRegion.cpp
+++ src/RefinementAllRegion.cpp
@@ -1,21 +1,27 @@
 /*
- <Mix-mesher: region type. This program generates a mixed-elements mesh>
- 
- Copyright (C) <2013,2017>  <Claudio Lobos>
- 
+ <Mix-mesher: region type. This program generates a mixed-elements 2D mesh>
+
+ Copyright (C) <2013,2018>  <Claudio Lobos> All rights reserved.
+
  This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
+ it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
+ GNU Lesser General Public License for more details.
+
  You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/gpl.txt>
+ along with this program.  If not, see <http://www.gnu.org/licenses/lgpl.txt>
  */
+/**
+* @file RefinementAllRegion.cpp
+* @author Claudio Lobos, Fabrice Jaillet
+* @version 0.1
+* @brief
+**/
 
 #include "RefinementAllRegion.h"
 
@@ -24,10 +30,11 @@ namespace Clobscode
 	//--------------------------------------------------------------------------------
 	//--------------------------------------------------------------------------------
 	RefinementAllRegion::RefinementAllRegion(const unsigned short &level)
+        :RefinementRegion(level)
 	{
-		refine_level = level;
-        local_rot = false;
-        input_rot = false;
+//		refine_level = level;
+//        local_rot = false;
+//        input_rot = false;
         
         //Use force input rotation (Defined in parent class) if the input domain
         //should be re-aligned.
--- src/RefinementRegion.cpp
+++ src/RefinementRegion.cpp
@@ -1,34 +1,42 @@
 /*
- <Mix-mesher: region type. This program generates a mixed-elements mesh>
- 
- Copyright (C) <2013,2017>  <Claudio Lobos>
- 
+ <Mix-mesher: region type. This program generates a mixed-elements 2D mesh>
+
+ Copyright (C) <2013,2018>  <Claudio Lobos> All rights reserved.
+
  This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
+ it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
+ GNU Lesser General Public License for more details.
+
  You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/gpl.txt>
+ along with this program.  If not, see <http://www.gnu.org/licenses/lgpl.txt>
  */
+/**
+* @file RefinementRegion.h
+* @author Claudio Lobos, Fabrice Jaillet
+* @version 0.1
+* @brief
+**/
 
 #include "RefinementRegion.h"
 
 namespace Clobscode
 {
+//unsigned short refine_level;
+//bool local_rot, input_rot;
+//vector<Point3D> pts;
+//GeometricTransform gt;
+
 	//--------------------------------------------------------------------------------
 	//--------------------------------------------------------------------------------
-	RefinementRegion::RefinementRegion()
-	{
-		refine_level = 0;
-        local_rot = false;
-        input_rot = false;
-	}
+    RefinementRegion::RefinementRegion(unsigned short level, bool local_rot, bool input_rot )
+        :refine_level(level),local_rot(local_rot),input_rot(local_rot)
+	{	}
 	//--------------------------------------------------------------------------------
 	//--------------------------------------------------------------------------------
 	RefinementRegion::~RefinementRegion()
