208bc96fce3188665707d3868c8184758a4b2900
Lafoix Paul <paul.lafoix@etu.univ-lyon1.fr>
2019-02-15 15:34:01 +0100

    prototype of meanTime working, only for transition patterns

--- src/MeanTimePerformance/Main.cpp
+++ src/MeanTimePerformance/Main.cpp
@@ -4,7 +4,8 @@
 #include <stdexcept>
 #include <string>
 #include <array>
-#include <vector>
+#include <map>
+#include <regex>
 
 std::string exec(const char* cmd) {
     std::array<char, 128> buffer;
@@ -21,19 +22,45 @@ std::string exec(const char* cmd) {
 
 void endMsg(){
     std::cout << std::flush;
-    std::cerr << "use: ./meanTime number\n";
+    std::cerr << "use: ./meanTime number [t]\n";
     std::cerr << "where:\n";
     std::cerr << "number is the number of times we execute mesher_roi program\n";
+    std::cerr << "Then at least one of the following list:\n";
+    std::cerr << "t : to compute the mean of the process Transition Patterns\n";
+    //TODO add doc
 }
 
-void fillVectorToEvaluate(char arg, std::vector<std::string>& vec) {
+void fillMapToEvaluate(char arg, std::map<std::string, float>& map) {
+
+    //Always look total :
+    map["Generation done"] = 0.f;
+
+    //TODO add options for every process...
+    //Or check all every time?
     switch(arg) {
         case 't': {
-            vec.push_back("Transition Patterns");
+            map["Transition Patterns"] = 0.f;
             break;
         }
         default: break;
     }
+}
+
+void addCorrespondingTime(const std::string& ouputOfMesher, std::map<std::string, float>& map) {
+
+    //Parse output and find lines that contain a word that is a key of map
+    //Then store corresponding time (after ' in ')
+
+    std::string processPattern; //Example "Transition Patterns"
+    std::smatch match;
+    for(auto& kv : map) {
+        processPattern = kv.first;
+        std::regex rgx(processPattern + " in ([0-9]+)");
+        std::regex_search(ouputOfMesher.begin(), ouputOfMesher.end(), match, rgx);
+        //TODO Check if match
+        map[processPattern] += std::stoi(match[1].str().c_str());
+    }
+
 }
 
 int main(int argc, char const *argv[])
@@ -50,19 +77,38 @@ int main(int argc, char const *argv[])
 
     std::string arguments = argv[2];
     char arg;
-    std::vector<std::string> processToEvaluate;
+    std::map<std::string, float> mapProcessToTime;
     for(unsigned i = 0; i < arguments.size(); ++i) {
         arg = arguments[i];
-        fillVectorToEvaluate(arg, processToEvaluate);
+        fillMapToEvaluate(arg, mapProcessToTime);
     }
 
     /*Debugging
-    for (std::vector<std::string>::iterator i = processToEvaluate.begin(); i != processToEvaluate.end(); ++i)
+    for (std::vector<std::string>::iterator i = mapProcessToTime.begin(); i != mapProcessToTime.end(); ++i)
     {
         std::cout << *i << std::endl;
     }
     std::cout << number << std::endl;
     */
+
+    for (int i = 0; i < number; ++i)
+    {
+        std::string output = exec(cmd);
+        addCorrespondingTime(output, mapProcessToTime);
+    }
+
+    //Divide to get the mean
+    float nb = (float) number;
+    for(auto& kv : mapProcessToTime) {
+        mapProcessToTime[kv.first] /= nb;
+    }
+
+    std::cout << "Test ended. After " << number << " executions, here are the results :\n";
+    for(auto& kv : mapProcessToTime) {
+        std::cout << " * " << kv.first << " in " << kv.second << " ms" << std::endl;
+    }
+
+
 
 
     return 0;
