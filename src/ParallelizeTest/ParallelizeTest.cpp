f069c687a3aa7ee6a7e6287c94264b8d01e27c93
antoine <antoine.o@outlook.com>
2019-02-20 17:57:00 +0100

    Improve container init time

--- src/ParallelizeTest/ParallelizeTest.cpp
+++ src/ParallelizeTest/ParallelizeTest.cpp
@@ -22,10 +22,6 @@
 
 using namespace std;
 
-/* Global */
-
-int NOMBRE_ELEM = 10000000;
-
 /*------------- Class -------------*/
 
 class Element;
@@ -51,11 +47,13 @@ public:
     }
 
     void divide(int n) {
+        //for(int i = 0; i < 10000; i++)
         val /= n;
     }
 
     void multiply(int n) {
-        val *= n;
+        for (int i = 0; i < 10000; i++)
+            val *= n;
     }
 
     const int value() const {
@@ -81,6 +79,12 @@ public:
             element.multiply(coef);
     };
 };
+
+/* Global */
+
+int NOMBRE_ELEM = 10000000;
+
+vector<Element> elements;
 
 /*------------- Init -------------*/
 
@@ -113,14 +117,14 @@ void init_list(list<Element> &list) {
 
 // VECTOR
 
-void simple_vector(vector<Element> &v, VisitorTest& visitor) {
+void simple_vector(vector<Element> &v, VisitorTest &visitor) {
     for (int i = 0; i < v.size(); i++) {
         visitor.visit(v[i]);
     }
 }
 
-void openmp_vector(vector<Element> &v, VisitorTest& visitor) {
-    #pragma omp parallel for
+void openmp_vector(vector<Element> &v, VisitorTest &visitor) {
+#pragma omp parallel for
     for (int i = 0; i < v.size(); i++) {
         visitor.visit(v[i]);
         // cout << v[i].value() << " ";
@@ -129,15 +133,15 @@ void openmp_vector(vector<Element> &v, VisitorTest& visitor) {
 
 // LIST
 
-void simple_list(list<Element> &l, VisitorTest& visitor) {
+void simple_list(list<Element> &l, VisitorTest &visitor) {
     auto iter = l.begin();
     for (; iter != l.end(); iter++) {
         visitor.visit(*iter);
     }
 }
 
-void openmp_list_copy(list<Element> &l, VisitorTest& visitor) {
-    std::vector<Element*> elements;
+void openmp_list_copy(list<Element> &l, VisitorTest &visitor) {
+    std::vector<Element *> elements;
     for (auto iter = l.begin(); iter != l.end(); ++iter)
         elements.push_back(&(*iter));
 
@@ -148,49 +152,47 @@ void openmp_list_copy(list<Element> &l, VisitorTest& visitor) {
     }
 }
 
-void openmp_list_copy2(list<Element> &l, VisitorTest& visitor) {
-    std::vector<Element*> items;
+void openmp_list_copy2(list<Element> &l, VisitorTest &visitor) {
+    std::vector<Element *> items;
 
     items.reserve(l.size());
     //put the pointers in the vector
-    transform(l.begin(), l.end(), back_inserter(items), 
-                   [](Element& n){ return &n; }
+    transform(l.begin(), l.end(), back_inserter(items),
+              [](Element &n) { return &n; }
     );
 
     #pragma omp parallel for
-    for (int i = 0; i < items.size(); i++)
-    {
-      visitor.visit(*(items[i]));
+    for (int i = 0; i < items.size(); i++) {
+        visitor.visit(*(items[i]));
     }
 }
 
 
-void openmp_list_nowait(list<Element> &l, VisitorTest& visitor) {
+void openmp_list_nowait(list<Element> &l, VisitorTest &visitor) {
     list<Element>::iterator iter;
     #pragma omp parallel private(iter)
     {
         //Every thread has its own copy of the for loop
         //And each one has a copy of iter
-        for(iter = l.begin(); iter != l.end(); iter++)
-        {
-          #pragma omp single nowait
-          {
-            //Only one per iteration (single), and the other don't wait (nowait)
-            iter->accept(visitor);
-          }
+        for (iter = l.begin(); iter != l.end(); iter++) {
+        #pragma omp single nowait
+            {
+                //Only one per iteration (single), and the other don't wait (nowait)
+                iter->accept(visitor);
+            }
         }
     }
     //Very long solution, why ?
     // Maybe because simulated tasks are too short
 }
 
-void openmp_list_task(list<Element> &l, VisitorTest& visitor) {
+void openmp_list_task(list<Element> &l, VisitorTest &visitor) {
     #pragma omp parallel
     {
-        #pragma omp single
+    #pragma omp single
         {
-            for(auto iter = l.begin(); iter != l.end(); ++iter) {
-                #pragma omp task firstprivate(iter)
+            for (auto iter = l.begin(); iter != l.end(); ++iter) {
+    #pragma omp task firstprivate(iter)
                 iter->accept(visitor);
             }
         }
@@ -207,16 +209,17 @@ void vector_test() {
     float time;
 
     // Simple thread test
-    init_vector(v);
-    cout<<"Simple vector..   ";
+    v.assign(elements.begin(), elements.end());
+    cout << "Simple vector..   ";
     time = count_time(simple_vector, v, visitor);
-    cout<<" Done in "<<time<<" ms."<<endl;
+    cout << " Done in " << time << " ms." << endl;
 
     // OpenMP test
-    init_vector(v);
-    cout<<"OpenMP vector..   ";
+    v.clear();
+    v.assign(elements.begin(), elements.end());
+    cout << "OpenMP vector..   ";
     time = count_time(openmp_vector, v, visitor);
-    cout<<" Done in "<<time<<" ms."<<endl;
+    cout << " Done in " << time << " ms." << endl;
 }
 
 void list_test() {
@@ -225,22 +228,24 @@ void list_test() {
     float time;
 
     // Simple thread test
-    init_list(l);
-    cout<<"Simple list..   ";
+    l.assign(elements.begin(), elements.end());
+    cout << "Simple list..   ";
     time = count_time(simple_list, l, visitor);
-    cout<<" Done in "<<time<<" ms."<<endl;
+    cout << " Done in " << time << " ms." << endl;
 
     //OpenMP list copy
-    init_list(l);
-    cout<<"OpenMP list copy..   ";
+    l.clear();
+    l.assign(elements.begin(), elements.end());
+    cout << "OpenMP list copy..   ";
     time = count_time(openmp_list_copy, l, visitor);
-    cout<<" Done in "<<time<<" ms."<<endl;
+    cout << " Done in " << time << " ms." << endl;
 
     //OpenMP list copy2
-    init_list(l);
-    cout<<"OpenMP list copy2..   ";
+    l.clear();
+    l.assign(elements.begin(), elements.end());
+    cout << "OpenMP list copy2..   ";
     time = count_time(openmp_list_copy2, l, visitor);
-    cout<<" Done in "<<time<<" ms."<<endl;
+    cout << " Done in " << time << " ms." << endl;
 
 
     //VEry long solutions......
@@ -269,10 +274,12 @@ int main(int argc, char const *argv[]) {
     }
 
     cout << "Launching tests with " << NOMBRE_ELEM << " elements.\n";
-    
+
+    init_vector(elements);
+
     vector_test();
-    
+
     list_test();
-    
+
     return 0;
 }
