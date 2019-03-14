0fc6a3698588a41cc3a453e02a57fe57939766f1
OlAntoine <antoine.o@outlook.com>
2019-03-14 11:49:41 +0100

    Add nb elements / times graph

--- script/plot_time.py
+++ script/plot_time.py
@@ -8,6 +8,7 @@ def takeFirst(elem):
 
 
 data = {}
+data2 = {}
 
 onlyfiles = glob.glob("analyse_time_2019-03-07_14:39:03/time_size*.txt")
 
@@ -29,74 +30,146 @@ for file in onlyfiles:
                 if nb_elements not in data:
                     data[nb_elements] = {}
 
+                if nb_threads not in data2:
+                    data2[nb_threads] = {}
+
             else:
-                #Now like : "simple_list"
-                #And not like : "simple list"
                 type = words[0]
                 if type not in data[nb_elements]:
                     data[nb_elements][type] = {}
 
+                if type not in data2[nb_threads]:
+                    data2[nb_threads][type] = {}
+
                 time = float(words[1])
-                #time = float(words.pop(0))
                 
                 data[nb_elements][type][nb_threads] = time
-
-counter = 1
-
-#print(data[100])
+                data2[nb_threads][type][nb_elements] = time
 
 
-fig = plt.figure()
-fig.suptitle("Graph of average thread execution time for different number of elements", fontsize=16)
+def plot_threads_times():
+    counter = 1
 
-#Fullscreen:
-mng = plt.get_current_fig_manager()
-mng.resize(*mng.window.maxsize())
+    fig = plt.figure()
+    fig.suptitle("Graph of average thread execution time for different number of elements", fontsize=16)
 
-#List of (pyplot lines, label) -> for legend
-#do it once (same legend for everyone??)
-legendLines = []
-legendLabel = []
-done = False
+    #Fullscreen:
+    mng = plt.get_current_fig_manager()
+    mng.resize(*mng.window.maxsize())
 
-for nb_elements, nb_elements_values in sorted(data.items()):
+    #List of (pyplot lines, label) -> for legend
+    #do it once (same legend for everyone??)
+    legendLines = []
+    legendLabel = []
+    done = False
 
-    #100 elem = 0..
-    if nb_elements == 100:
-        continue
+    for nb_elements, nb_elements_values in sorted(data.items()):
 
-    #plt.figure(counter)
-    ax = plt.subplot(2,3,counter);
+        #100 elem = 0..
+        if nb_elements == 100:
+            continue
 
-    plt.title(str(nb_elements) + " elements.")
-    plt.ylabel("Execution time")
-    plt.xlabel("Number of threads")
+        #plt.figure(counter)
+        ax = plt.subplot(2, 3, counter)
 
-    for type, type_values in nb_elements_values.items():
-        #x = nombre de threads
-        x = list(type_values.keys())
-        x.sort()
+        plt.title(str(nb_elements) + " elements.")
+        plt.ylabel("Execution time (ms)")
+        plt.xlabel("Number of threads")
 
-        print(x)
+        for type, type_values in nb_elements_values.items():
+            #x = nombre de threads
+            x = list(type_values.keys())
+            x.sort()
 
-        if len(x) == len(type_values.items()):
-            lst = list(type_values.items())
-            lst.sort(key=takeFirst)
-            values = list()
-            for val in lst:
-                values.append(val[1])
-            l = plt.plot(x, values, '-3', label=type)
-            #For figure legend: 
-            if not done : 
-                legendLines.append(l)
-                legendLabel.append(type)
+            print(x)
 
-        for nb_threads, time in type_values.items():
-            print(nb_elements, type, nb_threads, time)
+            if len(x) == len(type_values.items()):
+                lst = list(type_values.items())
+                lst.sort(key=takeFirst)
+                values = list()
 
-    #plt.legend()
-    counter += 1
-    done = True
+                for val in lst:
+                    values.append(val[1])
+                l = plt.plot(x, values, '-x', label=type, markersize=15)
 
-ax.legend(bbox_to_anchor=(1.15,0.5), loc='lower left', borderaxespad=0.)
-plt.show()
+                #For figure legend:
+                if not done:
+                    legendLines.append(l)
+                    legendLabel.append(type)
+
+            for nb_threads in x:
+                print(nb_elements, type, nb_threads, type_values[nb_threads])
+
+        #plt.legend()
+        counter += 1
+        done = True
+
+    ax.legend(bbox_to_anchor=(1.15, 0.5), loc='lower left', borderaxespad=0.)
+    plt.show()
+
+
+def plot_elements_times():
+    counter = 1
+
+    fig = plt.figure()
+    fig.suptitle("Graph of the average execution time of a structure according to the number of elements for a different number of threads", fontsize=16)
+
+    #Fullscreen:
+    mng = plt.get_current_fig_manager()
+    mng.resize(*mng.window.maxsize())
+
+    #List of (pyplot lines, label) -> for legend
+    #do it once (same legend for everyone??)
+    legendLines = []
+    legendLabel = []
+    done = False
+
+    for nb_threads, nb_threads_values in sorted(data2.items()):
+
+        #plt.figure(counter)
+        ax = plt.subplot(2, 3, counter)
+
+        plt.title(str(nb_threads) + " threads.")
+        plt.ylabel("Execution time (ms)")
+        plt.xlabel("Number of elements")
+        #plt.yscale('log')
+        plt.xscale('log')
+
+        for type, type_values in nb_threads_values.items():
+            #x = nombre de threads
+            x = list(type_values.keys())
+            x.sort()
+
+            print(x)
+
+            if len(x) == len(type_values.items()):
+                lst = list(type_values.items())
+                lst.sort(key=takeFirst)
+                values = list()
+
+                for val in lst:
+                    values.append(val[1])
+                    
+                #x.pop(0)
+                #values.pop(0)
+
+                l = plt.plot(x, values, '-x', label=type, markersize=15)
+
+                #For figure legend:
+                if not done:
+                    legendLines.append(l)
+                    legendLabel.append(type)
+
+            for nb_elements in x:
+                print(nb_threads, type, nb_elements, type_values[nb_elements])
+
+        #plt.legend()
+        counter += 1
+        done = True
+
+        ax.legend()
+    plt.show()
+
+
+plot_elements_times()
+#plot_threads_times()
