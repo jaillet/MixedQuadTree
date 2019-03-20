8abcd1d77f7c34b39023b5ad1923fd20f525750e
Lafoix Paul <paul.lafoix@etu.univ-lyon1.fr>
2019-03-20 12:36:33 +0100

    add one thread

--- script/analyse_time.sh
+++ script/analyse_time.sh
@@ -10,9 +10,9 @@ FILE="$DIR/analyse_time.txt"
 
 mkdir -p $DIR
 #
-for SIZE in 100 1000 10000 100000 1000000 10000000 100000000 1000000000
+for SIZE in 10000 100000 1000000 10000000 100000000 1000000000
 do
-    for THREAD in 8 4 2
+    for THREAD in 8 4 2 1
     do
         echo "Compute for size $SIZE with $THREAD threads"
         RES=$(./$PROG $SIZE $THREAD)
