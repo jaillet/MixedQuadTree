b3cb92e51462b9cf0e07307fb8e7d082a8a1f954
Antoine OLEKSIAK <antoine.oleksiak@etu.univ-lyon1.fr>
2019-05-23 10:52:26 +0200

    Fix script

--- script/plot_mesher_time.py
+++ script/plot_mesher_time.py
@@ -14,42 +14,48 @@ nbTries = 10
 
 def getData(option):
     data = {}
+
     for nbThread in range(1,nbThreads+1):
     
         data[nbThread] = {}
         files = glob.glob(folderName + "analyse_mesher_" + option + "/thread_" + str(nbThread) + "/*")
+        files += glob.glob(folderName + "analyse_mesher_" + option + "/sequential.txt")
 
         for fileStr in files:
-            file = open(fileStr, 'r')
 
-            methodName = fileStr[:-4].split("/")[-1]
+            #if 'eduction' in fileStr or 'sequential' in fileStr:
+            if 'TBBV3' in fileStr or 'TBBV4' in fileStr or 'sequential' in fileStr:
 
-            #Sum of all time for each level
-            data[nbThread][methodName] = [0 for i in range(maxRL)]
+                file = open(fileStr, 'r')
 
-            for line in file:
-                line = line.split(' ')
-                if line[0] == "Level":
-                    data[nbThread][methodName][int(line[1])] += int(line[3])
+                methodName = fileStr[:-4].split("/")[-1]
+
+                #Sum of all time for each level
+                data[nbThread][methodName] = [0 for i in range(maxRL)]
+
+                for line in file:
+                    line = line.split(' ')
+                    if line[0] == "Level" and int(line[1]) < maxRL:
+                        data[nbThread][methodName][int(line[1])] += int(line[3])
 
         #Calculate mean
         for method in data[nbThread]:
-            data[nbThread][method] = [x / nbTries for x in data[nbThread][method]]
+            if method != "sequential":
+                data[nbThread][method] = [x / nbTries for x in data[nbThread][method]]
 
     #Get sequential
-    file = open(folderName + "analyse_mesher_" + option + "/sequential.txt")
-    data[0] = []
-    for line in file:
-        line = line.split(' ')
-        if line[0] == "Level":
-            data[0].append(line[3]) 
+    #file = open(folderName + "analyse_mesher_" + option + "/sequential.txt")
+    #data[0] = []
+    #for line in file:
+    #    line = line.split(' ')
+    #    if line[0] == "Level":
+    #        data[0].append(line[3])
 
     return data
 
 dataA = getData('a')
 dataS = getData('s')
 
-print(dataA[0])
 
 
 def plot(data, opt):
@@ -96,5 +102,67 @@ def plot(data, opt):
 
     plt.show()
 
-plot(dataA, 'a')
-plot(dataS, 's')
+
+#plot(dataA, 'a')
+#plot(dataS, 's')
+
+
+def plotMutex(data, opt):
+
+    levels = [i for i in range(maxRL)]
+
+    counter = 1
+
+    plt.figure(counter)
+    plt.title("Option -" + opt + " reduction version")
+    plt.ylabel("Execution time(ms)")
+    plt.xlabel("Level number")
+
+    seq_draw = False
+
+    #fig = plt.figure()
+    #fig.suptitle("Graph of average thread execution time for different number of elements", fontsize=16)
+
+    for nbThread in range(4,nbThreads+1,4):
+
+        #Fullscreen:
+        #mng = plt.get_current_fig_manager()
+        #mng.resize(*mng.window.maxsize())
+
+        for methodName, timesList in data[nbThread].items():
+
+            #print(timesList)
+
+            if seq_draw and "sequential" in methodName:
+                continue
+
+            if timesList[-1] == 0:
+                continue
+
+            label = methodName + " - " + str(nbThread) + " threads"
+
+            if "sequential" in methodName:
+                seq_draw = True
+                label = methodName
+
+            plt.plot(levels, timesList, '-x', label=label, markersize=15)
+            plt.text(levels[-1] + 0.5, timesList[-1], str(timesList[-1]))
+
+
+
+
+        #plot sequential
+        #There is a problem !! When plot sequential,
+        #all yvalues are not shown correctly
+        #WHY ????
+        #plt.plot(levels, data[0])
+        #plt.text(levels[-1], data[0][-1], str(data[0][-1]))
+
+        plt.legend()
+        counter += 1
+
+    plt.show()
+
+
+plotMutex(dataA, 'a')
+plotMutex(dataS, 's')
