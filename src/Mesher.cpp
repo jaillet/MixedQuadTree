8059aae2d757b6ef5beb4e5b2fec5bc9fc0021a3
Fabrice Jaillet <fjaillet@gmail.com>
2020-06-11 12:38:48 +0200

    FJA updating copyright year to 2020, following merge new feature angle and histogram

--- src/Mesher.cpp
+++ src/Mesher.cpp
@@ -1,7 +1,7 @@
 /*
  <Mix-mesher: region type. This program generates a mixed-elements 2D mesh>
  
- Copyright (C) <2013,2019>  <Claudio Lobos> All rights reserved.
+ Copyright (C) <2013,2020>  <Claudio Lobos> All rights reserved.
  
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
